94b44b36f9522c8603e8dafbe228a524af3981df
Lucas Rangel Cezimbra <lrcezimbra@hotmail.com>
2017-05-22 21:54:00 -0300

    Add model subscription

--- subscriptions/core/models.py
+++ subscriptions/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Subscription(models.Model):
+    GENDERS = (
+        ('F', 'Feminino'),
+        ('M', 'Masculino'),
+    )
+
+    name = models.CharField('nome', max_length=200)
+    email = models.EmailField('e-mail')
+    name_for_bib_number = models.CharField('nome para número de peito',
+                                           max_length=200)
+    gender = models.CharField('sexo', max_length=1, choices=GENDERS)
+    date_of_birth = models.DateField('data de nascimento')
+    city = models.CharField('cidade', max_length=100)
+    team = models.CharField('equipe', max_length=100)
+    shirt_size = models.CharField('tamanho da camiseta', max_length=25)
+    modality = models.CharField('modalidade', max_length=25)
--- subscriptions/core/tests.py
+++ subscriptions/core/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from subscriptions.core.models import Subscription
 
-# Create your tests here.
+class SubscriptionTest(TestCase):
+    def test_name_not_blank(self):
+        subscription = Subscription.objects.create(
+            name='Lucas Rangel Cezimbra',
+            email='lucas.cezimbra@gmail.com',
+            name_for_bib_number='lucas.cezimbra@gmail.com',
+            gender='M',
+            date_of_birth='1996-08-12',
+            city='Porto Alegre',
+            team='Sprint Final',
+        )
