80f7d703536d6e4348da057d8fb3c7bc08bc7478
Lucas Rangel Cezimbra <lrcezimbra@hotmail.com>
2017-06-07 22:06:15 -0300

    Refactor view export

--- subscriptions/core/views.py
+++ subscriptions/core/views.py
@@ -11,22 +11,31 @@ def export(request):
         return render(request, 'export.html', {'form': ExportForm()})
     elif request.method == 'POST':
         form = ExportForm(request.POST)
-        subscription_resource = SubscriptionResource(form.data.getlist('fields'))
-        response = HttpResponse()
-        content_disposition = "attachment; filename*=utf-8''{}"
-        if form.data.get('format') == 'xlsx':
-            file = subscription_resource.export().get_xlsx()
-            content_type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-            content_disposition = content_disposition.format('export.xlsx')
-        else:
-            file = subscription_resource.export().csv
-            response = HttpResponse(content=file)
-            content_type = 'text/csv'
-            content_disposition = content_disposition.format('export.csv')
-        response.content = file
-        response['Content-Type'] = content_type
-        response['Content-Disposition'] = content_disposition
-        return response
+        return export_file_response(form.data.get('format'),
+                                    form.data.getlist('fields'))
+
+def export_file_response(format, fields):
+    content_disposition = "attachment; filename*=utf-8''{}"
+    FORMATS = {
+        'csv': {
+            'Content-Type': 'text/csv',
+            'Content-Disposition': content_disposition.format('export.csv'),
+        },
+        'xlsx': {
+            'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            'Content-Disposition': content_disposition.format('export.xlsx')
+        },
+    }
+    subscription_resource = SubscriptionResource(fields)
+    response = HttpResponse()
+    if format == 'xlsx':
+        response.content = subscription_resource.export().get_xlsx()
+    else:
+        response.content = subscription_resource.export().csv
+
+    for key, value in FORMATS[format].items():
+        response[key] = value
+    return response
 
 def SubscriptionResource(include_list=[], *args, **kwargs):
     class SubscriptionResource(resources.ModelResource):
