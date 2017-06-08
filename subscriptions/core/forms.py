58d806c718a4a7920236d4b1d7a768875c33721f
Lucas Rangel Cezimbra <lrcezimbra@hotmail.com>
2017-06-08 00:10:18 -0300

    Update layout export.html

--- subscriptions/core/forms.py
+++ subscriptions/core/forms.py
@@ -6,6 +6,9 @@ class ExportForm(forms.Form):
                ('xlsx', 'XLSX'))
     FIELDS = [(field.name, field.verbose_name)
               for field in Subscription._meta.get_fields()]
-    format = forms.ChoiceField(label='Formato', widget=forms.RadioSelect,
+    format = forms.ChoiceField(label='Formato',
+			       widget=forms.RadioSelect,
                                choices=FORMATS)
-    fields = forms.MultipleChoiceField(label='Campos', choices=FIELDS)
+    fields = forms.MultipleChoiceField(label='Campos',
+				       widget=forms.SelectMultiple(attrs={'size':len(FIELDS)+1}),
+				       choices=FIELDS)
